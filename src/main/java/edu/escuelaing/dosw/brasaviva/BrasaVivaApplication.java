9da6ce2c804abeb2e79b0885a4e64e3e4cfefcca
Julian Morales <julian.morales-z@mail.escuelaing.edu.co>
2026-09-10 08:17:51 -0500

    Estructura inicial del proyecto - Brasa Viva

---
+++ src/main/java/edu/escuelaing/dosw/brasaviva/BrasaVivaApplication.java
@@ -0,0 +1,11 @@
+package edu.escuelaing.dosw.brasaviva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasaVivaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrasaVivaApplication.class, args);
+    }
+}
